aea0314ba09efc8076f4a7f7f4b015c3e1bf3a74
narapon <43005911+narapons@users.noreply.github.com>
2019-03-18 01:17:05 +0900

    Update main.php

--- src/Craft/main.php
+++ src/Craft/main.php
@@ -29,103 +29,100 @@ class main extends PluginBase implements Listener{
 			     }else{
 			        switch($args[0]){
 			             case "1":
-				     $this->CraftSystem($sender, $args[1], 336, 0, 4, 45, 0, "レンガブロック");
+				     $this->CraftSystem($sender, $args[1], 5, 0, 6, 53, 0, "オークの木の階段");
 			             break;
 			             
 			             case "2":
-				     $this->CraftSystem($sender, $args[1], 5, 0, 6, 53, 0, "クォーツブロックのハーフ");
+				     $this->CraftSystem($sender, $args[1], 4, 0, 6, 67, 0, "石の階段");
 			             break;
 			             
 			             case "3":
-				     $this->CraftSystem($sender, $args[1], 4, 0, 6, 67, 0, "クォーツブロックのハーフ");
+				     $this->CraftSystem($sender, $args[1], 45, 0, 6, 108, 0, "レンガの階段");
 			             break;
 			             
 			             case "4":
-				     $this->CraftSystem($sender, $args[1], 45, 0, 6, 108, 0, "クォーツブロックのハーフ");
+				     $this->CraftSystem($sender, $args[1], 98, 0, 6, 109 0, "石レンガの階段");
 			             break;
 			             
-			             case "5":
-				     $this->CraftSystem($sender, $args[1], 98, 0, 6, 109 0, "クォーツブロックのハーフ");
+				     case "5":
+				     $this->CraftSystem($sender, $args[1], 112, 0, 6, 114, 0, "レザーレンガの階段");
 			             break;
 			             
-				     case "6":
-				     $this->CraftSystem($sender, $args[1], 112, 0, 6, 114, 0, "クォーツブロックのハーフ");
+			             case "6":
+				     $this->CraftSystem($sender, $args[1], 24, 0, 6, 128, 0, "砂岩の階段");
 			             break;
 			             
 			             case "7":
-				     $this->CraftSystem($sender, $args[1], 24, 0, 6, 128, 0, "クォーツブロックのハーフ");
+				     $this->CraftSystem($sender, $args[1], 179, 0, 6, 180, 0, "赤砂岩の階段");
 			             break;
 			             
 			             case "8":
-				     $this->CraftSystem($sender, $args[1], 179, 0, 6, 180, 0, "クォーツブロックのハーフ");
+				     $this->CraftSystem($sender, $args[1], 5, 1, 6, 134, 0, "マツの木の階段");
 			             break;
 			             
 			             case "9":
-				     $this->CraftSystem($sender, $args[1], 5, 1, 6, 134, 0, "クォーツブロックのハーフ");
+				     $this->CraftSystem($sender, $args[1], 5, 2, 6, 135, 0, "シラカバの木の階段");
 			             break;
 			             
 			             case "10":
-				     $this->CraftSystem($sender, $args[1], 5, 2, 6, 135, 0, "クォーツブロックのハーフ");
+			             $this->CraftSystem($sender, $args[1], 5, 3, 6, 136, 0, "ジャングルの木の階段");
 			             break;
 			             
 			             case "11":
-			             $this->CraftSystem($sender, $args[1], 5, 3, 6, 136, 0, "クォーツブロックのハーフ");
+				     $this->CraftSystem($sender, $args[1], 5, 4, 6, 163, 0, "アカシアの木の階段");
 			             break;
 			             
 			             case "12":
-				     $this->CraftSystem($sender, $args[1], 5, 4, 6, 163, 0, "クォーツブロックのハーフ");
+				     $this->CraftSystem($sender, $args[1], 5, 5, 6, 164, 0, "ダークオークの木の階段");
 			             break;
 			             
 			             case "13":
-				     $this->CraftSystem($sender, $args[1], 5, 5, 6, 164, 0, "クォーツブロックのハーフ");
+				     $this->CraftSystem($sender, $args[1], 155, 0, 6, 156, 0, "クォーツブロックの階段");
 			             break;
 			             
 			             case "14":
-				     $this->CraftSystem($sender, $args[1], 155, 0, 6, 156, 0, "クォーツブロックのハーフ");
+				     $this->CraftSystem($sender, $args[1], 201, 0, 6, 203, 0, "プルプァブロックの階段");
 			             break;
 			             
 			             case "15":
-				     $this->CraftSystem($sender, $args[1], 201, 0, 6, 203, 0, "クォーツブロックのハーフ");
+				     $this->CraftSystem($sender, $args[1], 5, 0, 3, 158, 0, "オークの木のハーフ");
 			             break;
 			             
 			             case "16":
-				     $this->CraftSystem($sender, $args[1], 5, 0, 3, 158, 0, "クォーツブロックのハーフ");
-			             break;
-			             
-			             case "17":
-			             $this->CraftSystem($sender, $args[1], 5, 1, 3, 158, 1, "クォーツブロックのハーフ");
+			             $this->CraftSystem($sender, $args[1], 5, 1, 3, 158, 1, "マツの木のハーフ");
 				     break;
 			             
-			             case "18":
-			             $this->CraftSystem($sender, $args[1], 5, 2, 3, 158, 2, "クォーツブロックのハーフ");
+			             case "17":
+			             $this->CraftSystem($sender, $args[1], 5, 2, 3, 158, 2, "シラカバの木のハーフ");
 			             break;
 			             
-			             case "19":
-			             $this->CraftSystem($sender, $args[1], 5, 3, 3, 158, 3, "クォーツブロックのハーフ");
+			             case "18":
+			             $this->CraftSystem($sender, $args[1], 5, 3, 3, 158, 3, "ジャングルの木のハーフ");
 			             break;
 			                
-			             case "20":
-			             $this->CraftSystem($sender, $args[1], 5, 4, 3, 158, 4, "クォーツブロックのハーフ");
+			             case "19":
+			             $this->CraftSystem($sender, $args[1], 5, 4, 3, 158, 4, "アカシアの木のハーフ");
 			             break;
 			                   
-			             case "21":
-			             $this->CraftSystem($sender, $args[1], 5, 5, 3, 158, 5, "クォーツブロックのハーフ");
+			             case "20":
+			             $this->CraftSystem($sender, $args[1], 5, 5, 3, 158, 5, "ダークオークの木のハーフ");
 			             break;
 			              
+			             case "21":
+			             $this->CraftSystem($sender, $args[1], 98, 0, 3, 45, 5, "石レンガのハーフ");
+			             break;
+			             
 			             case "22":
-			             $this->CraftSystem($sender, $args[1], 98, 0, 3, 45, 5, "クォーツブロックのハーフ");
+			             $this->CraftSystem($sender, $args[1], 45, 0, 3, 44, 4, "レンガブロックのハーフ");
 			             break;
 			             
 			             case "23":
-			             $this->CraftSystem($sender, $args[1], 45, 0, 3, 44, 4, "クォーツブロックのハーフ");
-			             break;
-			             
-			             case "24":
 			             if(isset($args[1])){
 			                 $this->CraftSystem($sender, $args[1], 155, 0, 3, 44, 6, "クォーツブロックのハーフ");
-			             }else{
-			                 $sender->sendMessage("use: /carft <クラフトID> <個数>");
-			             }
+			             break;
+					     
+				     case "24":
+				     $this->CraftSystem($sender, $args[1], 336, 0, 4, 45, 0, "レンガブロック");
 			             break;
 			                
 			             case "list":
@@ -134,43 +131,45 @@ class main extends PluginBase implements Listener{
 			             }else{
 			                 switch($args[1]){
 			                     case "1":
-			                     $sender->sendMessage("§a<クラフト可能アイテム一覧>  <1/3>");
+			                     $sender->sendMessage("§a<クラフト可能アイテム一覧>  <1/5>");
 			                     $sender->sendMessage("§a| アイテム名 | 使用アイテム | クラフトID |");
-			                     $sender->sendMessage("| レンガブロック | レンガ×4 | 1 |");
-			                     $sender->sendMessage("| オークの木の階段 | 木材×6 | 2 |");
-			                     $sender->sendMessage("| 石の階段 | 丸石×6 | 3 |");
-			                     $sender->sendMessage("| レンガの階段 | レンガブロック×6 | 4 |");
-			                     $sender->sendMessage("| 石レンガの階段 | 石レンガブロック×6 | 5 |");
-			                     $sender->sendMessage("| レザーレンガの階段 | 暗黒レンガブロック×6 | 6 |");
-			                     $sender->sendMessage("| 砂岩の階段 | 砂岩×6 | 7 |");
-			                     $sender->sendMessage("| 赤砂岩の階段 | 赤砂岩×6 | 8 |");
+			                     $sender->sendMessage("| オークの木の階段 | 木材×6 | 1 |");
+			                     $sender->sendMessage("| 石の階段 | 丸石×6 | 2 |");
+			                     $sender->sendMessage("| レンガの階段 | レンガブロック×6 | 3 |");
+			                     $sender->sendMessage("| 石レンガの階段 | 石レンガブロック×6 | 4 |");
+			                     $sender->sendMessage("| レザーレンガの階段 | 暗黒レンガブロック×6 | 5 |");
+			                     $sender->sendMessage("| 砂岩の階段 | 砂岩×6 | 6 |");
+			                     $sender->sendMessage("| 赤砂岩の階段 | 赤砂岩×6 | 7 |");
 			                     break;
 			                     
 			                     case "2":
-			                     $sender->sendMessage("§a<クラフト可能アイテム一覧>  <2/3>");
+			                     $sender->sendMessage("§a<クラフト可能アイテム一覧>  <2/5>");
 			                     $sender->sendMessage("§a| アイテム名 | 使用アイテム | クラフトID |");
-			                     $sender->sendMessage("| マツの木の階段 | マツの木材×4 | 9 |");
-			                     $sender->sendMessage("| シラカバの木の階段 | シラカバの木材×6 | 10 |");
-			                     $sender->sendMessage("| ジャングルの木の階段 | ジャングルの木材×6 | 11 |");
-			                     $sender->sendMessage("| アカシアの木の階段 | アカシアの木材×6 | 12 |");
-			                     $sender->sendMessage("| ダークオークの木の階段 | ダークオーの木材×6 | 13 |");
-			                     $sender->sendMessage("| クォーツブロックの階段 | ネザー水晶×6 | 14 |");
-			                     $sender->sendMessage("| プルプァブロックの階段 | プルプァブロック×6 | 15 |");
+			                     $sender->sendMessage("| マツの木の階段 | マツの木材×4 | 8 |");
+			                     $sender->sendMessage("| シラカバの木の階段 | シラカバの木材×6 | 9 |");
+			                     $sender->sendMessage("| ジャングルの木の階段 | ジャングルの木材×6 | 10 |");
+			                     $sender->sendMessage("| アカシアの木の階段 | アカシアの木材×6 | 11 |");
+			                     $sender->sendMessage("| ダークオークの木の階段 | ダークオーの木材×6 | 12 |");
+			                     $sender->sendMessage("| クォーツブロックの階段 | ネザー水晶×6 | 13 |");
+			                     $sender->sendMessage("| プルプァブロックの階段 | プルプァブロック×6 | 14 |");
 			                     break;
 			                     
 			                     case "3":
-			                     $sender->sendMessage("§a<クラフト可能アイテム一覧>  <3/3>");
+			                     $sender->sendMessage("§a<クラフト可能アイテム一覧>  <3/5>");
 			                     $sender->sendMessage("§a| アイテム名 | 使用アイテム | クラフトID |");
-			                     $sender->sendMessage("| オークの木のハーフ | オークの木材×3 | 16 |");
-			                     $sender->sendMessage("| マツの木のハーフ | マツの木材×4 | 17 |");
-			                     $sender->sendMessage("| シラカバの木のハーフ | シラカバの木材×6 | 18 |");
-			                     $sender->sendMessage("| ジャングルの木のハーフ | ジャングルの木材×6 | 19 |");
-			                     $sender->sendMessage("| アカシアの木のハーフ | アカシアの木材×6 | 20 |");
-			                     $sender->sendMessage("| ダークオークの木のハーフ | ダークオーの木材×6 | 21 |");
-			                     $sender->sendMessage("| 石レンガのハーフ | 石レンガ×3 | 22 |");
-			                     $sender->sendMessage("| レンガブロックのハーフ | レンガブロック×3 | 23 |");
-			                     $sender->sendMessage("| クォーツブロックのハーフ | クォーツブロック×3 | 24 |");
+			                     $sender->sendMessage("| オークの木のハーフ | オークの木材×3 | 15 |");
+			                     $sender->sendMessage("| マツの木のハーフ | マツの木材×4 | 16 |");
+			                     $sender->sendMessage("| シラカバの木のハーフ | シラカバの木材×6 | 17 |");
+			                     $sender->sendMessage("| ジャングルの木のハーフ | ジャングルの木材×6 | 18 |");
+			                     $sender->sendMessage("| アカシアの木のハーフ | アカシアの木材×6 | 19 |");
+			                     $sender->sendMessage("| ダークオークの木のハーフ | ダークオーの木材×6 | 20 |");
+			                     $sender->sendMessage("| 石レンガのハーフ | 石レンガ×3 | 21 |");
+			                     $sender->sendMessage("| レンガブロックのハーフ | レンガブロック×3 | 22 |");
+			                     $sender->sendMessage("| クォーツブロックのハーフ | クォーツブロック×3 | 23 |");
 			                     break;
+					     
+					     case "4":
+				             $sender->sendMessage("| レンガブロック | レンガ×4 | 24 |");
 			                     
 			                     default:
 			                     $sender->sendMessage("use: /craft list <ページ数>");
